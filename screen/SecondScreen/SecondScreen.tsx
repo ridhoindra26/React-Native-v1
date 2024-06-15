bfa1bd23ada78551f5b11fd329ea418ccec72207
ridhoindra26 <indraridho26@gmail.com>
2024-06-15 10:42:39 +0700

    profile

--- screen/SecondScreen/SecondScreen.tsx
+++ screen/SecondScreen/SecondScreen.tsx
@@ -1,14 +1,117 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet ,View, Text, Image, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const SecondScreen = () => {
+
+  const handleEditPress = () => {
+
+  }
+
   return (
-    <View>
-      <Text>SecondScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Image
+          style={styles.coverPhoto}
+          source={{uri: 'https://marketplace.canva.com/EAElz5XZDf8/1/0/1600w/canva-header-twitter-banner-foto-kota-yIjGvVWBsoY.jpg'}}
+        />
+        <View style={styles.profileContainer}>
+          <Image
+            style={styles.profilePhoto}
+            source={{uri: 'https://www.bootdey.com/img/Content/avatar/avatar1.png'}}
+          />
+          <Text style={styles.nameText}>Your Name</Text>
+        </View>
+      </View>
+      <View style={styles.bioContainer}>
+        <Text style={styles.bioText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+          ullamcorper nisi.
+        </Text>
+      </View>
+      <View style={styles.statsContainer}>
+        <View style={styles.statContainer}>
+          <Text style={styles.statCount}>1234</Text>
+          <Text style={styles.statLabel}>Posts</Text>
+        </View>
+        <View style={styles.statContainer}>
+          <Text style={styles.statCount}>5678</Text>
+          <Text style={styles.statLabel}>Followers</Text>
+        </View>
+        <View style={styles.statContainer}>
+          <Text style={styles.statCount}>9101</Text>
+          <Text style={styles.statLabel}>Following</Text>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleEditPress}>
+        <Text style={styles.buttonText}>Edit Profile</Text>
+      </TouchableOpacity>
+    </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  headerContainer: {
+    alignItems: 'center',
+  },
+  coverPhoto: {
+    width: '100%',
+    height: 100,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginTop: -50,
+  },
+  profilePhoto: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  nameText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    color: 'black',
+  },
+  bioContainer: {
+    padding: 15,
+  },
+  bioText: {
+    fontSize: 16,
+    color: 'black',
+  },
+  statsContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  statContainer: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  statCount: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  statLabel: {
+    fontSize: 16,
+    color: '#999',
+  },
+  button: {
+    backgroundColor: '#0066cc',
+    borderRadius: 5,
+    padding: 10,
+    marginHorizontal: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
 
 export default SecondScreen
